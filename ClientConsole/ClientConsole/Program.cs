0c9cb4376fa457c206ab4371e098da3fd0b5eea3
Hitesh Kumar <bhardwajhiteshkumar01@gmail.com>
2018-08-14 10:11:35 +0530

    Initial commit

---
+++ ClientConsole/ClientConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientConsole
+{
+    class Program
+    {
+        public static int Main(String[] args)
+        {
+            SynchronousSocketClient.StartClient();
+            return 0;
+        }
+    }
+}
---
+++ ServerConsole/ServerConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConsole
+{
+    class Program
+    {
+        public static int Main(String[] args)
+        {
+            SynchronousSocketListener.StartListening();
+            return 0;
+        }
+    }
+}
